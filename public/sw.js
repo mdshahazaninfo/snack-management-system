8e1c6f41adb7649e3a4f58a684b89314819e758c
mdshahazaninfo <mdshahazan.info@gmail.com>
2026-07-22 01:04:05 +0600

    Refresh PWA cache for mobile layout

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,32 @@
-const CACHE = 'snackflow-v1'
+const CACHE = 'snackflow-v3'
 const APP = '/snack-management-system/'
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll([APP, `${APP}manifest.webmanifest`, `${APP}icons/icon.svg`]))))
-self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))))
+
+self.addEventListener('install', event => {
+  self.skipWaiting()
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll([
+    APP,
+    `${APP}manifest.webmanifest`,
+    `${APP}icons/icon.svg`,
+  ])))
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim()),
+  )
+})
+
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return
-  event.respondWith(fetch(event.request).then(response => {
-    const copy = response.clone()
-    caches.open(CACHE).then(cache => cache.put(event.request, copy))
-    return response
-  }).catch(() => caches.match(event.request).then(hit => hit || caches.match(APP))))
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone()
+        caches.open(CACHE).then(cache => cache.put(event.request, copy))
+        return response
+      })
+      .catch(() => caches.match(event.request).then(hit => hit || caches.match(APP))),
+  )
 })
